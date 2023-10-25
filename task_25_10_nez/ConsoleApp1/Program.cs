6ebb099e5481fe72f68caddddcf140bdf16c5fe1
unknown <mi779pf7t@code.edu.az>
2023-10-25 10:36:10 +0300

    first commit

---
+++ task_25_10_nez/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using Models.Entities;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Weapon silah = new Weapon(28,15,10,"tekli");
+
+        int key = -1;
+        Console.Write("CapacityOfMagazine: ");
+        int capacityOfMagazine = Convert.ToInt32(Console.ReadLine());
+        silah.CapacityOfMagazine = capacityOfMagazine;
+
+        Console.Write("CountOfBullet: ");
+        int countOfBullet = Convert.ToInt32(Console.ReadLine());
+        silah.CountOfBullet = countOfBullet;
+
+        Console.Write("TimeOfMagazine: ");
+        float timeOfMagazine = Convert.ToInt32(Console.ReadLine());
+        silah.TimeOfMagazine = timeOfMagazine;
+
+        Console.Write("FireMode: ");
+        string fireMode = Console.ReadLine();
+        silah.FireMode = fireMode;
+
+        Console.WriteLine
+    (
+    "0 - Informasiya almaq ucun\n" +
+    "1 - Shoot metodu ucun\n" +
+    "2 - Fire metodu ucun\n" +
+    "3 - GetNeededBulletCount metodu ucun\n" +
+    "4 - Reload metodu ucun\n" +
+    "5 - ChangeFireMode metodu ucun metodu ucun\n" +
+    "6 - Proqrami dayandirmaq ucun\n" +
+    "7 - Yeni silah yaratmaq ucun\n"
+    );
+
+        do
+        {
+            key = Convert.ToInt32(Console.ReadLine());
+            if (key == 0) // Informasiya almaq ucun
+            {
+                silah.GetFullInfo();
+            }
+            if (key == 1) // Shoot metodu ucun
+            {
+                silah.Shoot();
+            }
+            if (key == 2) // Fire metodu ucun
+            {
+                silah.Fire();
+            }
+            if (key == 3) // GetNeededBulletCount metodu ucun
+            {
+                silah.GetNeededBulletCount();
+            }
+            if (key == 4) // Reload metodu ucun
+            {
+                silah.Reload();
+            }
+            if (key == 5) // ChangeFireMode metodu ucun
+            {
+                silah.ChangeFireMode();
+            }
+            if (key == 7)
+            {
+                Console.Write("CapacityOfMagazine: ");
+                capacityOfMagazine = Convert.ToInt32(Console.ReadLine());
+                silah.CapacityOfMagazine = capacityOfMagazine;
+
+                Console.Write("CountOfBullet: ");
+                countOfBullet = Convert.ToInt32(Console.ReadLine());
+                silah.CountOfBullet = countOfBullet;
+
+                Console.Write("TimeOfMagazine: ");
+                timeOfMagazine = Convert.ToInt32(Console.ReadLine());
+                silah.TimeOfMagazine = timeOfMagazine;
+
+                Console.Write("FireMode: ");
+                fireMode = Console.ReadLine();
+                silah.FireMode = fireMode;
+            }
+        }
+
+        while (key != 6 && key <= 7 && key >= -1);
+        {
+
+        }
+    }
+}
---
+++ task_25_10_nez/Models/Entities/Weapon.cs
@@ -0,0 +1,104 @@
+﻿using System.IO;
+
+namespace Models.Entities;
+
+public class Weapon
+{
+    private int _capacityOfMagazine;
+    public Weapon(int _capacityOfMagazine, int _countOfBullet, float _timeOfMagazine, string _fireMode)
+    {
+        CapacityOfMagazine = _capacityOfMagazine;
+        CountOfBullet = _countOfBullet;
+        TimeOfMagazine = _timeOfMagazine;
+        FireMode = _fireMode;
+    }
+
+    public int CapacityOfMagazine
+    {
+        get => _capacityOfMagazine;
+        set { _capacityOfMagazine = value; }
+    }
+
+    public int CountOfBullet
+    {
+        get;
+        set;
+    }
+
+    public float TimeOfMagazine
+    {
+        get ;
+        set ;
+    }
+
+    public string FireMode
+    {
+        get;
+        set;
+    }
+
+    public int Shoot() 
+    {
+        return CountOfBullet--;
+    }
+
+    public int Fire() 
+    {
+        float time = 0;
+        float timePerBullet = TimeOfMagazine/CountOfBullet;   
+        for (int i = CountOfBullet; i >0; i--)
+        {
+            if (FireMode == "avtomatik")
+            {
+                CountOfBullet--;
+                time = TimeOfMagazine;
+            }
+            else if (FireMode == "tekli")
+            {
+                CountOfBullet--;
+                time =+ timePerBullet;
+                Console.WriteLine("1 gulle atildi");
+                break;
+            }
+
+        }
+        Console.WriteLine($"{time} saniyeye bitdi");
+        return CountOfBullet;
+    }
+
+    public void GetNeededBulletCount()
+    {
+        Console.WriteLine($"Daragin tam dolmasi ucun lazim olan gulle sayi: {CapacityOfMagazine - CountOfBullet}"); 
+    }
+
+    public void Reload()
+    {
+        for (int i = CountOfBullet; i < CapacityOfMagazine; i++)
+        {
+            CountOfBullet++;
+        }
+        Console.WriteLine($"daraq yeniden doldu. Gulle sayi: {CountOfBullet}"); 
+    }
+
+    public string ChangeFireMode() 
+    { 
+        if ( FireMode == "tekli" )
+        {
+            FireMode = "avtomatik";
+        }
+        else if ( FireMode == "avtomatik")
+        {
+            FireMode = "tekli";
+        }
+        else
+        {
+            Console.WriteLine("atis modu yanlis daxil edilib!");
+        }
+        return FireMode;
+    }
+
+    public void GetFullInfo()
+    {
+        Console.WriteLine($"{CapacityOfMagazine} {CountOfBullet} {TimeOfMagazine} {FireMode}");
+    }
+}
