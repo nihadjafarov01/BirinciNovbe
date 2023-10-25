67fbd08a20a9d1e83ff36c76941761ced17f1a80
unknown <mi779pf7t@code.edu.az>
2023-10-25 12:50:05 +0300

    first commit

---
+++ task_25_10_company_copy/Company_Project/Company.cs
@@ -0,0 +1,57 @@
+﻿namespace Company_Project;
+
+public class Company
+{
+    string _name;
+    public string Name
+    {
+        get
+        {
+            return this.Name;
+        }
+        set
+        {
+
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                value = value.Trim();
+                this._name = value.ToUpper()[0] + value.Substring(1);
+            }
+        }
+    }
+    public Employee[] Employees; //new Employee[0]
+
+    public Company(string name)
+    {
+        Name = name;
+        Employees = new Employee[0];
+    }
+
+    public void AddUser(Employee employee)
+    {
+        Array.Resize(ref Employees, Employees.Length + 1);
+        Employees[Employees.Length - 1] = employee;
+        Console.WriteLine("Successfully added!");
+    }
+    public Employee GetUser(string username)
+    {
+        foreach (Employee emp in Employees)
+        {
+            Console.WriteLine(emp.Name);
+            Console.WriteLine(emp.Surname);
+            Console.WriteLine(emp.Username);
+
+            if (emp.Username == username) {  return emp; }
+        }
+        Console.WriteLine("Employee with given username not found");
+        return null;
+    }
+
+    public void GetAllUser()
+    {
+        foreach (Employee item in Employees)
+        {
+            Console.WriteLine(item.Username);
+        }
+    }
+}
