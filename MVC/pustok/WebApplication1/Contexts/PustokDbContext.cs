c532a9722686d693340e94d0f45ec14db2f50314
unknown <mi779pf7t@code.edu.az>
2023-11-30 13:38:09 +0400

    added pustok

---
+++ MVC/pustok/WebApplication1/Contexts/PustokDbContext.cs
@@ -0,0 +1,16 @@
+﻿using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Contexts
+{
+    public class PustokDbContext : DbContext
+    {
+        public DbSet<Slider> Sliders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-41FVTKL\\SQLEXPRESS; Database = Pustok; Trusted_Connection=True;Encrypt=false;TrustServerCertificate=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        
+    }
+}
---
+++ MVC/pustok/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebApplication1.Contexts;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        
+        public async Task<IActionResult> Index()
+        {
+            using PustokDbContext context = new PustokDbContext();
+            var sliders = await context.Sliders.ToListAsync();
+            return View(sliders);
+        }
+
+        
+    }
+}
